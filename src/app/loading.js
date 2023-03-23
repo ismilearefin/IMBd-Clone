dcf146bdc22e690ff7e3b58770682ba08d0c9084
ismilearefin <arefinismile@gmail.com>
2023-03-24 03:06:43 +0600

    Movie page

--- src/app/loading.js
+++ src/app/loading.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function loading() {
   return (
     <div className='mt-24 flex justify-center items-center'>
-        <img className="w-28" src="spinner.svg" alt="loading..."/>
+        <img className="w-12" src="spinner.svg" alt="loading..."/>
     </div>
   )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import { BiLike } from 'react-icons/bi';
 
 
 export default function Card({result}) {
-    // console.log(result)
+    
   return (
     <div className="group cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border border-slate-400 sm:m-2 transition-shadow duration-200 ">
         <Link href={`/movie/${result.id}`}>
